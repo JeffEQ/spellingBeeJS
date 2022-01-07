ac4a37d616793da5e3d906ef37c55e84dd2a9ce9
Jeff <mr.jeffreysam@gmail.com>
2022-01-06 19:22:07 -0600

    Completed step-4, working APP

--- script.js
+++ script.js
@@ -122,4 +122,22 @@ f_button.addEventListener("click", (event) => {
 //add a function to take your word from the word div and add it to an Array to then add to your Words Bank
 
 
+const wordsBankArray= [];
+let wordCount = 0;
+foundTitle.innerHTML = `You have found ${wordCount} words!`;
+wordsBankDIV.appendChild(foundTitle);
 
+addButton.addEventListener('click', (e) => {
+    e.preventDefault();
+wordsBankArray.push(wordDIV.innerHTML);
+console.log("wordsBankArray", wordsBankArray);
+
+let newWordDIv = document.createElement("div");
+  newWordDIv.setAttribute('id', `${wordCount}`);
+  newWordDIv.classList.add("is-size-2");
+  newWordDIv.innerHTML = wordsBankArray[wordCount];
+  wordsBankDIV.appendChild(newWordDIv);
+  wordCount++;
+  foundTitle.innerHTML = `You have found ${wordCount} words!`;
+  wordDIV.innerHTML="";
+})
