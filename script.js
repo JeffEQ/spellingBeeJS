2aa1dbac5e153a3982b6bde4d85cb380625218e7
Jeff <mr.jeffreysam@gmail.com>
2022-01-06 18:10:59 -0600

    merged Step-2 into Step-3

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 //get the div items in the game
 const gameBoardDIV = document.getElementById('gameboard');
 const loadingBeeDIV = document.getElementById('loadingBee');
-const gameDIV = document.getElemenetByID('game');
+const gameDIV = document.getElementById('game');
 const wordDIV = document.getElementById('word');
-const LettersDIV = document.getElementById('letters');
+const lettersDIV = document.getElementById('letters');
 
 
 // get the div items in the wordsBank
@@ -34,30 +34,48 @@ d_button.classList.add("button", "is-warning", "m-2");
 d_button.setAttribute("value", "D");
 d_button.innerHTML = "D";
 
-const a_button = document.createElement('button');
+const e_button = document.createElement('button');
 e_button.classList.add("button", "is-warning", "m-2");
 e_button.setAttribute("value", "E");
 e_button.innerHTML = "E";
 
-const a_button = document.createElement('button');
+const f_button = document.createElement('button');
 f_button.classList.add("button", "is-warning", "m-2");
 f_button.setAttribute("value", "F");
 f_button.innerHTML = "F";
 
 const addButton =document.createElement('div');
-addButton.classList.add("button", "is -black");
-
+addButton.classList.add("button", "is-black");
+addButton.innerHTML ="ADD";
 
 
 //write the function to add the buttons to your HTML on page load
 //** NOTE: setTimeout is just to slow the load so you can see it. but you could use it to add a CSS loaders and other neat stuff!! :)
 
+document.addEventListener("DOMContentLoaded", () => {
+    setTimeout(()=>{
+     gameDIV.appendChild(addButton);
+    }, 3500) 
+});
 //using EventListener "DOMContentLoaded", load the add buttons
 
 
 //using an IIFE, load the letter button
 //Why did I put the letters in the IFFE??  HINT: LOOK AT THE CONSOLE LOG AND SEE WHEN IT FIRES.
+(() => {
+setTimeout(() => {
+loadingBeeDIV.classList.toggle("hide");
+wordDIV.classList.toggle("hide");
+wordsBankDIV.classList.toggle("hide");
+lettersDIV.appendChild(a_button);
+lettersDIV.appendChild(b_button);
+lettersDIV.appendChild(c_button);
+lettersDIV.appendChild(d_button);
+lettersDIV.appendChild(e_button);
+lettersDIV.appendChild(f_button);
 
+}, 3000)
+})();
 
 //Step-3 write the listeners to take a letter and add to your word div
 
